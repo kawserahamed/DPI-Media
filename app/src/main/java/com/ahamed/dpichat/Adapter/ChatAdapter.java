d89131721469ab7277f78def9e34ae47fb897440
kawser <kawserarasel24@gmail.com>
2022-08-02 07:55:32 +0600

    All Done

---
+++ app/src/main/java/com/ahamed/dpichat/Adapter/ChatAdapter.java
@@ -0,0 +1,81 @@
+package com.ahamed.dpichat.Adapter;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ahamed.dpichat.Model.ChatModel;
+import com.ahamed.dpichat.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+
+    FirebaseUser currentUser;
+    static final int RIGHT = 0;
+    static final int LEFT = 1;
+
+    Context context;
+    List<ChatModel> chatList;
+
+    public ChatAdapter(Context context, List<ChatModel> chatList) {
+        this.context = context;
+        this.chatList = chatList;
+    }
+
+    @NonNull
+    @Override
+    public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == RIGHT) {
+            View rightView = LayoutInflater.from(context).inflate(R.layout.layout_right_chat, parent, false);
+            return new ChatViewHolder(rightView);
+        } else {
+            View leftView = LayoutInflater.from(context).inflate(R.layout.left_chat, parent, false);
+            return new ChatViewHolder(leftView);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
+        ChatModel model = chatList.get(position);
+        holder.message.setText(model.getMessage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String currentId = null;
+        if (currentUser != null) {
+            currentId = currentUser.getUid();
+        }
+        if (chatList.get(position).getSenderId().equals(currentId)) {
+            return RIGHT;
+        } else {
+            return LEFT;
+        }
+
+
+    }
+
+    static class ChatViewHolder extends RecyclerView.ViewHolder {
+        TextView message;
+
+        public ChatViewHolder(@NonNull View itemView) {
+            super(itemView);
+            message = itemView.findViewById(R.id.tvChatMessage);
+        }
+    }
+}
--- app/src/main/java/com/ahamed/dpichat/Fragment/MessageFragment.java
+++ app/src/main/java/com/ahamed/dpichat/Fragment/MessageFragment.java
@@ -7,16 +7,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.ahamed.dpichat.Adapter.ChatAdapter;
+import com.ahamed.dpichat.Model.ChatModel;
 import com.ahamed.dpichat.Model.ProfileModel;
 import com.ahamed.dpichat.UI.DashboardActivity;
 import com.ahamed.dpichat.databinding.FragmentMessageBinding;
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 
 public class MessageFragment extends Fragment {
@@ -24,6 +34,8 @@ public class MessageFragment extends Fragment {
     FirebaseAuth auth;
     FirebaseDatabase database;
     DatabaseReference databaseReference;
+    DatabaseReference chatDatabase;
+    List<ChatModel> chatList;
 
     public MessageFragment() {
         // Required empty public constructor
@@ -39,14 +51,19 @@ public class MessageFragment extends Fragment {
         auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference("Chat");
-        ProfileModel model = MessageFragmentArgs.fromBundle(getArguments()).getProfileData();
+        ProfileModel otherProfile = MessageFragmentArgs.fromBundle(getArguments()).getProfileData();
+        chatList = new ArrayList<>();
+        ProfileModel myProfile = DashboardActivity.model;
+
+        Glide.with(binding.getRoot()).load(otherProfile.getImageUrl()).into(binding.ivOtherProfile);
+        binding.ivOtherName.setText(otherProfile.getName());
 
 
         binding.btnSend.setOnClickListener(view -> {
             String massageId = databaseReference.push().getKey();
-            String myId = DashboardActivity.model.getId();
+            String myId = myProfile.getId();
             String strMessage = binding.textMessage.getText().toString();
-            String strReceiver = model.getId();
+            String strReceiver = otherProfile.getId();
 
             if (TextUtils.isEmpty(strMessage)) {
                 binding.textMessage.setError("Can't be empty");
@@ -58,12 +75,43 @@ public class MessageFragment extends Fragment {
                 chatMap.put("messageId", massageId);
 
                 assert massageId != null;
-                databaseReference.child(massageId).setValue(chatMap).addOnCompleteListener(task -> {
-                    binding.textMessage.setText("");
-                }).addOnFailureListener(e -> Toast.makeText(getActivity(), "" + e.getMessage(), Toast.LENGTH_SHORT).show());
+                databaseReference.child(massageId).setValue(chatMap).addOnCompleteListener(task ->
+                        binding.textMessage.setText("")).addOnFailureListener(e ->
+                        Toast.makeText(getActivity(), "" + e.getMessage(), Toast.LENGTH_SHORT).show());
             }
         });
 
+
+        chatDatabase = FirebaseDatabase.getInstance().getReference("Chat");
+
+        chatDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                chatList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    ChatModel chatModel = dataSnapshot.getValue(ChatModel.class);
+
+                    assert chatModel != null;
+                    if (otherProfile.getId().equals(chatModel.getSenderId())
+                            && chatModel.getReceiverId().equals(myProfile.getId())
+                            || chatModel.getSenderId().equals(myProfile.getId())
+                            && chatModel.getReceiverId().equals(otherProfile.getId())) {
+                        chatList.add(chatModel);
+                    }
+                }
+                ChatAdapter adapter = new ChatAdapter(getActivity(), chatList);
+                binding.RcMessageViewId.setLayoutManager(new LinearLayoutManager(getActivity()));
+                binding.RcMessageViewId.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
         return binding.getRoot();
     }
+
 }
