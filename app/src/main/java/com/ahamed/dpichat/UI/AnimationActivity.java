d70d4cbdcd64da730445bdfd494867e97e698a06
kawser <kawserarasel24@gmail.com>
2022-08-11 22:35:07 +0600

    Animation added

--- app/src/main/java/com/ahamed/dpichat/UI/AnimationActivity.java
+++ app/src/main/java/com/ahamed/dpichat/UI/AnimationActivity.java
@@ -1,6 +1,8 @@
 package com.ahamed.dpichat.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +10,27 @@ import com.ahamed.dpichat.R;
 
 public class AnimationActivity extends AppCompatActivity {
 
+    CountDownTimer countDownTimer;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
+
+        countDownTimer= new CountDownTimer(3000,100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+
+            }
+        }.start();
     }
 }
